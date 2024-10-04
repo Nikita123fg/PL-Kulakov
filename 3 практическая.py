df108fb53c34bc98bb781933eea4cbb7a36dc7a3
Nikita123fg <nkulakov313@gmail.com>
2024-10-04 21:33:44 +0300

    Update 3 практическая.py

--- 3 практическая.py
+++ 3 практическая.py
@@ -1,6 +1,6 @@
 def can_break_chocolate(n, m, k):
     parts = n * m
-    return (k < total_pieces) and (k % n == 0 or k % m == 0)
+    return (k < parts) and (k % n == 0 or k % m == 0)
 
 n, m, k = map(int, input("Введите n, m и k через пробел: ").split())
 if can_break_chocolate(n, m, k):
